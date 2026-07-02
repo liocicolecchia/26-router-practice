1812da986fdabb6af468f4b457b2b89039299f02
Lio <liocicolecchia@gmail.com>
2026-07-03 00:21:48 +0200

    adding DUMMY_EVENTS and maping them to a list on the event page. using Link from react router to make the URL the id of the event

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Some event",
+  },
+  {
+    id: "e1",
+    title: "Another event",
+  },
+];
+
 function Events() {
-  return <h1>Events</h1>;
+  return (
+    <>
+      <h1>Events</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default Events;
