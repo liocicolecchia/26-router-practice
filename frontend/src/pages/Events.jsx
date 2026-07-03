0838a61264e316d56af1aac78f6c5d0f3dd474f8
Lio <liocicolecchia@gmail.com>
2026-07-04 01:26:05 +0200

    changeing the code tho throw Response.json() wich helps because you dont have to parse when using the useRouteError object

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -24,9 +24,11 @@ export async function loader() {
 
   if (!response.ok) {
     // return { isError: true, message: "Could not fetch the events." };
-    throw new Response(JSON.stringify({ message: "Could not fetch events. " }), {
-      status: 500,
-    });
+    // throw new Response(JSON.stringify({ message: "Could not fetch events. " }), {
+    //   status: 500,
+    // });
+
+    throw Response.json({ message: "Could not fetch events. " }, { status: 500 });
   } else {
     return response;
   }
