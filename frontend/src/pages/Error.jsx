17158211a7c35f368302bbb7bd8abf016cf63012
Lio <liocicolecchia@gmail.com>
2026-07-04 01:12:15 +0200

    how to use throw new Response for error handling to use the useRouteError feature of react-router. Helps you build a dynamic error component based on the status code of the error

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,5 +1,27 @@
+import { useRouteError } from "react-router";
+
+import PageContent from "../components/PageContent";
+
 function Error() {
-  return <h1>An error occurred!</h1>;
+  const error = useRouteError();
+
+  let title = "An error occurred!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resource or page.";
+  }
+
+  return (
+    <PageContent title={title}>
+      <p>{message}</p>
+    </PageContent>
+  );
 }
 
 export default Error;
