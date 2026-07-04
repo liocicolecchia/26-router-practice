456e4c043198d19a85c3fa11843bb61ae74fc089
Lio <liocicolecchia@gmail.com>
2026-07-05 01:52:42 +0200

    adding the add new event functionality. using the EventForm component the collect the event inputs and using the react-router Form/redirect and action to work with the form input data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { RouterProvider, createBrowserRouter } from "react-router";
 import Home from "./pages/Home";
 import Events, { loader as eventLoader } from "./pages/Events";
 import EventDetail, { loader as eventDetailLoader } from "./pages/EventDetail";
-import NewEvent from "./pages/NewEvent";
+import NewEvent, { action as newEventAction } from "./pages/NewEvent";
 import EditEvent from "./pages/EditEvent";
 import RootLayout from "./pages/RootLayout";
 import EventsRoot from "./pages/EventsRoot";
@@ -34,7 +34,7 @@ const router = createBrowserRouter([
               { path: "edit", element: <EditEvent /> },
             ],
           },
-          { path: "new", element: <NewEvent /> },
+          { path: "new", element: <NewEvent />, action: newEventAction },
         ],
       },
     ],
--- frontend/src/pages/NewEvent.jsx
+++ frontend/src/pages/NewEvent.jsx
@@ -1,3 +1,5 @@
+import { redirect } from "react-router";
+
 import EventForm from "../components/EventForm";
 
 function NewEvent() {
@@ -5,3 +7,28 @@ function NewEvent() {
 }
 
 export default NewEvent;
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw Response.json({ message: "Could not save event." }, { status: 500 });
+  }
+
+  return redirect("/events");
+}
