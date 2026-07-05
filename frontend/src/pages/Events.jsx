20a01a31d53e8d1e3bc15aca6c6b909653fa094a
Lio <liocicolecchia@gmail.com>
2026-07-05 18:45:47 +0200

    how to implement a kind of lazy load that allows you to already render a part of the page event if for example not all data fetching has already happened. for this you use Suspense form react and Await form react-router

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,25 +1,24 @@
-import { useLoaderData } from "react-router";
+import { Suspense } from "react";
+
+import { useLoaderData, Await } from "react-router";
 
 import EventsList from "../components/EventsList";
 
 function Events() {
-  const data = useLoaderData();
-  const events = data.events;
-
-  // if (data.isError) {
-  //   return <p>{data.message}</p>;
-  // }
+  const { events } = useLoaderData();
 
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default Events;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -30,6 +29,13 @@ export async function loader() {
 
     throw Response.json({ message: "Could not fetch events. " }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export async function loader() {
+  return {
+    events: loadEvents(),
+  };
 }
