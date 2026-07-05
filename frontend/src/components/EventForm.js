68a6216d9b76a63acdc5581df2a53e55b72893e5
Lio <liocicolecchia@gmail.com>
2026-07-05 17:46:06 +0200

    using react-router useNavigation to disable the form buttons and button text while the new event is beeing submitted to the backend

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,9 +1,14 @@
-import { Form, useNavigate } from "react-router";
+import { Form, useNavigate, useNavigation } from "react-router";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
+
   function cancelHandler() {
     navigate("..");
   }
@@ -51,10 +56,10 @@ function EventForm({ method, event }) {
         />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>{isSubmitting ? "Submitting..." : "Save"}</button>
       </div>
     </Form>
   );
