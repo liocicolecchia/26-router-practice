873ea50c3246d22498eb32147f013dc3177db562
Lio <liocicolecchia@gmail.com>
2026-07-05 18:11:16 +0200

    changeing the new event action in a way that i can use it as a action for edit event and new event. Taking the action from new event to the event Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,12 @@ import EventDetail, {
   loader as eventDetailLoader,
   action as deleteEventAction,
 } from "./pages/EventDetail";
-import NewEvent, { action as newEventAction } from "./pages/NewEvent";
+import NewEvent from "./pages/NewEvent";
 import EditEvent from "./pages/EditEvent";
 import RootLayout from "./pages/RootLayout";
 import EventsRoot from "./pages/EventsRoot";
 import Error from "./pages/Error";
+import { action as manipulateEventAction } from "./components/EventForm";
 
 const router = createBrowserRouter([
   {
@@ -34,10 +35,10 @@ const router = createBrowserRouter([
             loader: eventDetailLoader,
             children: [
               { index: true, element: <EventDetail />, action: deleteEventAction },
-              { path: "edit", element: <EditEvent /> },
+              { path: "edit", element: <EditEvent />, action: manipulateEventAction },
             ],
           },
-          { path: "new", element: <NewEvent />, action: newEventAction },
+          { path: "new", element: <NewEvent />, action: manipulateEventAction },
         ],
       },
     ],
